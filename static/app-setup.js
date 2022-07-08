614ab1ad1d0dae09e47137aa977a27d18f874b9b
Navneet720 <navneetpandey7208@gmail.com>
2022-07-08 15:33:39 +0100

    css and js files

---
+++ static/app-setup.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDiQG5cDQhTQ2On_eFsn-mv_kqgaLhQMX8",
+    authDomain: "web-project-f6093.firebaseapp.com",
+    projectId: "web-project-f6093",
+    storageBucket: "web-project-f6093.appspot.com",
+    messagingSenderId: "231575281923",
+    appId: "1:231575281923:web:c98a496be7dd9e347c8936"
+  };
+
+firebase.initializeApp(firebaseConfig);
